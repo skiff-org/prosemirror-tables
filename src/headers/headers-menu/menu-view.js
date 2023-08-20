de07ea7fad9f7787c6d2b8e3e2df723640b1c85b
Andrew Milich <milichab@gmail.com>
2023-08-19 19:27:52 -0700

    version bump - tab

--- src/headers/headers-menu/menu-view.js
+++ src/headers/headers-menu/menu-view.js
@@ -5,6 +5,7 @@ import {TextField} from './textField/text-field.prosemirror';
 import {tableHeadersMenuKey} from '../../columnsTypes/types.config';
 import {columnTypesMap} from '../../columnsTypes/types.config';
 import {createElementWithClass, getBaseName} from '../../util';
+import {goToNextCell} from '../../commands';
 
 /**
  * class attached to the editor and update table tooltip on every view update.
@@ -67,9 +68,16 @@ class TableHeadersMenuView {
         e.preventDefault();
         e.stopPropagation();
         console.log('tab');
-        const {tr} = this.view.state;
-        tr.setMeta(tableHeadersMenuKey, {action: 'close', id: window.id});
-        this.view.dispatch(tr);
+
+        // Use the goToNextCell command to shift focus to the next cell
+        if (goToNextCell(1)(this.view.state, this.view.dispatch)) {
+          // If the next cell exists and focus is shifted, then we don't need to do anything further.
+        } else {
+          // close menu - no more cells
+          const {tr} = this.view.state;
+          tr.setMeta(tableHeadersMenuKey, {action: 'close', id: window.id});
+          this.view.dispatch(tr);
+        }
       }
     });
 
